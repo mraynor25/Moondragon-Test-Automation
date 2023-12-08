689c95beb6d7a5b825d07f5a509b5b9e20470fc0
mraynor25 <mraynor@us.globalinfotek.com>
2023-12-08 13:56:39 -0500

    ioc lists userrole dec 8 23

--- TestCases/test_IOCLists_UserRole.py
+++ TestCases/test_IOCLists_UserRole.py
@@ -11,7 +11,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 import HtmlTestRunner
 
-# pass test sept 25 2023 modified
+# pass test Dec 8 2023
 
 class Test_IOCList_UserRole(unittest.TestCase):
     username = "analyst"
